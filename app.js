baad399e62eb59b7e1150c78f26ccb6e4f2bf022
Lucas Lima <lucasvinilima15@gmail.com>
2022-04-11 20:08:00 -0300

    Conectando ao mongodb

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 
-const alunoRouter = require('./routes/aluno');
+mongoose.connect("mongodb://localhost/cantina-online");
+const db = mongoose.connection;
+db.on("error", (error) => console.log(error));
+db.once("open", () => console.log("Connected to Database"));
+
+const alunoRouter = require("./routes/aluno");
 
 // app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/aluno', alunoRouter);
+app.use("/aluno", alunoRouter);
 
 app.listen(3000);
