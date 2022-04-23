4cb1f74928e9e22ccee7be159f8bf70802e33856
Lucas Lima <lucasvinilima15@gmail.com>
2022-04-22 22:50:49 -0300

    documentando product handler

--- src/handlers/product-handler.js
+++ src/handlers/product-handler.js
@@ -1,17 +1,38 @@
 const ProductService = require("../services/product-service")
 
 class ProductHandler {
+	/**
+	 * @route /product
+	 * @method GET
+	 * 
+	 * Retorna json com todos os produtos do banco
+	 * 
+	 * @returns [products] json
+	 */
 	static async getProducts(req, res) {
 		const response = await ProductService.searchProduct()
 
 		return res.json(response)
 	}
 
-	static postProduct(req, res) {
+	/**
+	 * @route /product
+	 * @method POST
+	 * 
+	 * Salva um produto no banco de dados
+	 * 
+	 * @body
+	 * @requires
+	 * @String name
+	 * @Number price
+	 * @String category 
+	 * 
+	 * @returns product json
+	 */
+	static async postProduct(req, res) {
 		const product = req.body
-		console.log(product)
 
-		const response = ProductService.save(product)
+		const response = await ProductService.save(product)
 
 		return res.json(response)
 	}
