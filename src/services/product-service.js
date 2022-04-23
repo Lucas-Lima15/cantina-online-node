86355388f9f6f054fe995b88d5e0caa0bda9467e
Lucas Lima <lucasvinilima15@gmail.com>
2022-04-22 22:32:54 -0300

    documentando product-service

--- src/services/product-service.js
+++ src/services/product-service.js
@@ -1,11 +1,28 @@
 const Product = require("../models/product")
 
+/**
+ * Contém serviços relacionados ao produto
+ */
 class ProductService {
+	
+
+	/**
+	 * Consulta todos os produtos no banco de dados
+	 * 
+	 * @returns [products]
+	 */
 	static async searchProduct() {
 		const products = await Product.find()
 		return products.length ? products : "Nenhum produto cadastrado"
 	}
 
+
+	/**
+	 * Salva um produto no banco de dados
+	 * 
+	 * @param {*} prod 
+	 * @returns product
+	 */
 	static async save(prod) {
 		const product = new Product(prod)
 		await product.save(product)
