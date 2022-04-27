fc967c04744f714e0852f201e2f68bf2432809b5
Lucas Lima <lucasvinilima15@gmail.com>
2022-04-27 01:18:02 -0300

    concertando teste unitario

--- src/tests/unit/product-service.test.js
+++ src/tests/unit/product-service.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 /* eslint-disable no-undef */
 const ProductService = require("../../services/product-service")
 const Product = require("../../models/product")
@@ -5,26 +6,25 @@ const mongoose = require("mongoose")
 const productMock = require("../mock/product")
 
 describe("ProductService", () => {
-	beforeAll(async () => {
+	beforeAll(() => {
 		mongoose.connect("mongodb://localhost:27017/cantina-online-test")
 		const db = mongoose.connection
 		db.on("error", console.error.bind(console, "connection error: "))
-		db.once("open", function() {
-			console.log("Connected successfully")
-		})
+	})
+
+	afterAll(() => {
+		mongoose.connection.close()
 	})
 
 	beforeEach(async () => {
 		await Product.deleteMany()
 	})
 
-	it("searchProduct", async () => {
+	it("Should return saved products in database", async () => {
 		await Product.insertMany(productMock)
 
 		const products = await ProductService.searchProduct()
 
-		console.log(products[0].name)
-        
-		expect(products[0]).toBe(productMock)
+		expect(JSON.stringify(products[0])).toBe(JSON.stringify(productMock))
 	})
 })
